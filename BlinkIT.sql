a6cbbbe30fb8c89b2697a13c798d26f9445131ff
Mainak <162096806+Mainak2005@users.noreply.github.com>
2025-06-14 21:19:39 +0530

    Update BlinkIT.sql

--- BlinkIT.sql
+++ BlinkIT.sql
@@ -1,4 +1,12 @@
 SELECT * FROM BlinkIT_data
+  UPDATE blinkit_data
+SET Item_Fat_Content = 
+    CASE 
+        WHEN Item_Fat_Content IN ('LF', 'low fat') THEN 'Low Fat'
+        WHEN Item_Fat_Content = 'reg' THEN 'Regular'
+        ELSE Item_Fat_Content
+    END;
+
 SELECT Outlet_Location_Type , Item_Fat_Content, 
 CAST(SUM(Total_Sales) AS DECIMAL (10,2)) AS  Total_Sales,
 CAST(AVG(Total_Sales) AS DECIMAL(10,2)) AS Avg_Sales,
@@ -8,3 +16,44 @@ FROM BlinkIT_data
 WHERE Outlet_Establishment_Year = 2020
 GROUP BY Outlet_Location_Type, Item_Fat_Content 
 ORDER BY Total_Sales ASC
+
+SELECT Outlet_Location_Type, 
+       ISNULL([Low Fat], 0) AS Low_Fat, 
+       ISNULL([Regular], 0) AS Regular
+FROM 
+(
+    SELECT Outlet_Location_Type, Item_Fat_Content, 
+           CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+    FROM blinkit_data
+    GROUP BY Outlet_Location_Type, Item_Fat_Content
+) AS SourceTable
+PIVOT 
+(
+    SUM(Total_Sales) 
+    FOR Item_Fat_Content IN ([Low Fat], [Regular])
+) AS PivotTable
+ORDER BY Outlet_Location_Type;
+
+
+SELECT 
+    Outlet_Size, 
+    CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+    CAST((SUM(Total_Sales) * 100.0 / SUM(SUM(Total_Sales)) OVER()) AS DECIMAL(10,2)) AS Sales_Percentage
+FROM blinkit_data
+GROUP BY Outlet_Size
+ORDER BY Total_Sales DESC;
+
+SELECT Outlet_Location_Type, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Outlet_Location_Type
+ORDER BY Total_Sales DESC
+
+SELECT Outlet_Type, 
+CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+		CAST(AVG(Total_Sales) AS DECIMAL(10,0)) AS Avg_Sales,
+		COUNT(*) AS No_Of_Items,
+		CAST(AVG(Rating) AS DECIMAL(10,2)) AS Avg_Rating,
+		CAST(AVG(Item_Visibility) AS DECIMAL(10,2)) AS Item_Visibility
+FROM blinkit_data
+GROUP BY Outlet_Type
+ORDER BY Total_Sales DESC
